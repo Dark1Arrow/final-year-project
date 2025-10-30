b8f34131cdc092a2a1df5deec514a1f3fcdc5722
Goutam khanna <123886841+Dark1Arrow@users.noreply.github.com>
2025-10-30 12:05:52 +0530

    fix: removed geist font and added postcss plugin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,19 @@
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }) {
+export const metadata: Metadata = {
+  title: "Final Year Project",
+  description: "Created by Dark1Arrow",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>

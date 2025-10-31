f086bc6516f0b72b4f712b5c61c38de9aaf5a39a
Goutam khanna <123886841+Dark1Arrow@users.noreply.github.com>
2025-10-31 12:57:03 +0530

    testing

--- app/api/index.js
+++ app/api/index.js
@@ -1,15 +1,34 @@
-import ConnectDB from "./db/index.js";
-import dotenv from "dotenv"
-import {app} from "./app.js";
+// import ConnectDB from "./db/index.js";
+// import dotenv from "dotenv"
+// import {app} from "./app.js";
+
+// dotenv.config();
+
+// ConnectDB()
+// .then(() => {
+//     app.listen(process.env.PORT || 3000 , ()=>{
+//         console.log(`Server is running at port number : ${process.env.PORT}`)
+//     })
+// })
+// .catch((err)=>{
+//     console.log(`MongoDB connection failed !!!! : ${err}`)
+// })
+
+// api/index.js
+import app from "../app.js";
+import ConnectDB from "../db/index.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-ConnectDB()
-.then(() => {
-    app.listen(process.env.PORT || 3000 , ()=>{
-        console.log(`Server is running at port number : ${process.env.PORT}`)
-    })
-})
-.catch((err)=>{
-    console.log(`MongoDB connection failed !!!! : ${err}`)
-})
+let isConnected = false;
+
+export default async function handler(req, res) {
+  if (!isConnected) {
+    await ConnectDB();
+    isConnected = true;
+    console.log("MongoDB Connected ✅");
+  }
+
+  return app(req, res);
+}

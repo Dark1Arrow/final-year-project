f88b4caf0a20e4bef76f6397e74212a8edc28e81
Goutam khanna <123886841+Dark1Arrow@users.noreply.github.com>
2025-10-31 14:19:42 +0530

    testing

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-import { app } from "../app/api/app.js";
-import ConnectDB from "../app/api/db/index.js";
+import app from "../app.js";
+import ConnectDB from "../db/index.js";
 import dotenv from "dotenv";
 
 dotenv.config();

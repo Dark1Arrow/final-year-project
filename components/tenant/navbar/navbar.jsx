c1d74970e95f911d283a467b8e66dfeb6142c735
Goutam khanna <123886841+Dark1Arrow@users.noreply.github.com>
2025-11-01 14:32:44 +0530

    testing

--- components/tenant/navbar/navbar.jsx
+++ components/tenant/navbar/navbar.jsx
@@ -1,7 +1,6 @@
 "use client"
 import React, { useState } from 'react';
 import axios from 'axios';
-import { process.env.NEXT_PUBLIC_API_URL } from '';
 
 const LogoutIcon = (props) => (
   <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-out">
